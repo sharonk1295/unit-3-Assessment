dc27f2f2b5f10377927e2331ac1e16803accdca5
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-06-15 11:58:07 -0400

    solved first algorithm

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // console.log('test');
+
+// Given an integer, return an integer that is the reverse
+
+const reverseInt = (num) => {
+    let numArr = num.toString().split('') // turn # into string and then store in array 
+    numArr = parseInt(numArr.reverse().join('')); // reverse the order of the array and then turn back into a string, which then gets parsed into an integer
+    // Math.sign() returns either a pos or neg 1
+    return numArr * Math.sign(num);
+}
+
+console.log(reverseInt(-15));

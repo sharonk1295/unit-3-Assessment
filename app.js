51d9071f34a145b2ddfa10336ef99be498d42c18
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-06-15 13:22:12 -0400

    solved second algorithm

--- app.js
+++ app.js
@@ -9,4 +9,30 @@ const reverseInt = (num) => {
     return numArr * Math.sign(num);
 }
 
-console.log(reverseInt(-15));
+// console.log(reverseInt(-15));
+
+
+// Given a string, return the character that is most commonly used in the string
+const maxChar = (string) => {
+    let countObj = {};
+    let newArr = string.split('');
+    // for (let i = 0; i < newArr.length; i++) {
+    //     countObj[i] = (countObj[i] || 0) + 1
+    // } // returned the index number as key and the count which was 1
+    for (char of newArr) {
+        countObj[char] = (countObj[char] || 0) + 1
+    } // returns each letter as a key and the count for each letter, but still returns the whole countObj object
+    let maxCount = 0; // frequency count starts off at 0
+    let freqChar; // this will be assigned a value based on which character has the maxCount
+    for (character in countObj) {
+        if (countObj[character] > maxCount) {
+            maxCount = countObj[character];
+            freqChar = character;
+        } //else {
+        //    freqChar = 'they all have equal count'
+        //} // returns this phrase when theres an even count for each letter, but once I add numbers, it only prints this, even though a single number clearly had the max count
+    }
+    return freqChar;
+}
+
+console.log(maxChar('apple 123111111'))
